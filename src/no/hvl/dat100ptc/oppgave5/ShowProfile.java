78d72bcf8524117de0f2fbf15ebb432e85b34bba
h672645 <111350084+h672645@users.noreply.github.com>
2022-10-17 02:01:34 +0200

    opp

--- src/no/hvl/dat100ptc/oppgave5/ShowProfile.java
+++ src/no/hvl/dat100ptc/oppgave5/ShowProfile.java
@@ -59,6 +59,8 @@ public class ShowProfile extends EasyGraphics {
 			
 		}
 		
+		
+		
 	}
 
 }
--- src/no/hvl/dat100ptc/oppgave5/ShowRoute.java
+++ src/no/hvl/dat100ptc/oppgave5/ShowRoute.java
@@ -1,6 +1,9 @@
 package no.hvl.dat100ptc.oppgave5;
 
+import static javax.swing.JOptionPane.showInputDialog;
+
 import javax.swing.JOptionPane;
+import static java.lang.String.*;
 
 import easygraphics.EasyGraphics;
 import no.hvl.dat100ptc.TODO;
@@ -62,15 +65,12 @@ public class ShowRoute extends EasyGraphics {
 		return ystep;
 	}
 
-	
 	public void showRouteMap(int ybase) {
-		
-		
+
 		double[] speeds = gpscomputer.speeds();
 		System.out.println(speeds.length);
 		System.out.println(gpspoints.length);
-		
-		
+
 		double minlon = GPSUtils.findMin(GPSUtils.getLongitudes(gpspoints));
 		double minlat = GPSUtils.findMin(GPSUtils.getLatitudes(gpspoints));
 		double[] x = new double[gpspoints.length];
@@ -86,41 +86,43 @@ public class ShowRoute extends EasyGraphics {
 
 		}
 		
-		setColor(0,0,255);
-//		int a = circle((int) (x[0] + 0.5), ybase - (int) (y[0] + 0.5),3);
+		setSpeed(10);
+		
+		setColor(0, 0, 255);
+		int a = circle((int) (x[0] + 0.5), ybase - (int) (y[0] + 0.5),3);
 		int speedTellar = 1;
 		for (int i = 0; i < gpspoints.length; i++) {
 			int tempY = (int) (y[i] + 0.5);
 			int tempX = (int) (x[i] + 0.5);
-			if (x[i] == x[gpspoints.length-1]) {
+			if (x[i] == x[gpspoints.length - 1]) {
 				setColor(0, 0, 255);
 				fillCircle(MARGIN + tempX, ybase - tempY, 5);
 			} else {
 				setColor(0, 255, 0);
 				fillCircle(MARGIN + tempX, ybase - tempY, 2);
 			}
-			
-//			moveCircle(a, MARGIN + tempX , ybase - tempY);
+
+			moveCircle(a, MARGIN + tempX , ybase - tempY);
 
 			if (i > 0) {
 				int tempYForige = (int) (y[i - 1] + 0.5);
 				int tempXForige = (int) (x[i - 1] + 0.5);
-//
-//				if (gpspoints[i].getElevation() < gpspoints[i - 1].getElevation()) {
-//					setColor(255, 0, 0);
-//				} else {
-//					setColor(0, 255, 0);
-//				}
-//				
-				drawLine(MARGIN + tempX, ybase - tempY, MARGIN + tempXForige, ybase - tempYForige);
-//				
-//				int speedGraph = (int)(speeds[i-1]);
-//				drawLine(650 + xTellar, 75, 650 + xTellar, 75-speedGraph );
-//				xTellar += 1;
+
+				if (gpspoints[i].getElevation() < gpspoints[i - 1].getElevation()) {
+					setColor(255, 0, 0);
+					drawLine(MARGIN + tempX, ybase - tempY, MARGIN + tempXForige, ybase - tempYForige);
+				} else {
+					setColor(0, 255, 0);
+					drawLine(MARGIN + tempX, ybase - tempY, MARGIN + tempXForige, ybase - tempYForige);
+				}
+
+				int speedGraph = (int) (speeds[i - 1]);
+				drawLine(650 + xTellar, 75, 650 + xTellar, 75 - speedGraph);
+				xTellar += 1;
 			}
-			
+
 		}
-		
+
 	}
 
 	public void showStatistics() {
@@ -148,10 +150,19 @@ public class ShowRoute extends EasyGraphics {
 
 	public int circle(int centerX, int centerY, int radius) {
 
-		
 		int circle = fillCircle(MARGIN + centerX, centerY, radius);
-		
+
 		return circle;
+	}
+
+	private static int input(String message) {
+		int antall = 0;
+		
+		
+		String innTxt = showInputDialog(message);
+		antall = Integer.parseInt(innTxt);
+		
+		return antall;
 	}
 	
 }
